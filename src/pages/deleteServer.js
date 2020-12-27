0148ac8b855bf38f59f3aaf88baafa25d4b44fc8
Phobiac01 <phobiac01@gmail.com>
2020-12-26 18:32:48 -0700

    Added back buttons & svg import

--- src/pages/deleteServer.js
+++ src/pages/deleteServer.js
@@ -1,3 +1,13 @@
+import { Link } from "react-router-dom";
+import backSVG from "../assets/back-icon.svg";
+
 export default function DeleteServer() {
-  return <h1>DeleteServer Works!</h1>;
+  return (
+    <div>
+      <Link to={`/`}>
+        <img src={backSVG} alt="Back Button" />
+      </Link>
+      <h1>DeleteServer Works!</h1>
+    </div>
+  );
 }
--- src/pages/updateServer.js
+++ src/pages/updateServer.js
@@ -1,3 +1,14 @@
+import { Link } from "react-router-dom";
+import backSVG from "../assets/back-icon.svg";
+
 export default function UpdateServer() {
-    return <h1>UpdateServer Works!</h1>
+  return (
+    <div>
+      <Link to={`/`}>
+        <img src={backSVG} alt="Back Button" />
+      </Link>
+      <br />
+      <h1>UpdateServer Works!</h1>
+    </div>
+  );
 }
